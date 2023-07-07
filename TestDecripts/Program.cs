637a683110813f3f8f9ccbaca9bfbd8fda6eed6b
Maikol José Martínez Sanjuán <maikol.martinez@softwareone.com>
2023-07-07 18:13:03 -0500

    Test console

--- TestDecripts/Program.cs
+++ TestDecripts/Program.cs
@@ -38,9 +38,9 @@ namespace TestDecripts
 
             try
             {
-                string keyEjemplo = "x4zEU9yKqtmdBV1X9zwIW8lwhbjjfpqf9vU/6g7T9nrYxRfVwdJEofzXnRpfCB6s/3InvcMYUOEco91SjjzOVSHvUjExVszLnYoXP5h+V/JqHImqWfA+p6ywlvxQxzhiqC7EBQ9rOB7EF/DsRYQZgQJSIHnlTiZBEk//X080O/F6UhrPfZCYD7oNf8VNFzXdaeuSfi2XohTVPBz8UdXO0sQm3RMNE5OukjAYf7GEdasO7wqAtzMIuPPQSIF1WHxtuYSm903+I3NFIYgb/djrl2FssdGaKRWMrfZDaBAtQn9TDpdk2l/ZtNDE0Eh6EqNhLlohkcQf6gEui2qvptT8g5YbgMbhqRBxlffyL8gIVO2r3FzWyDf3ApBSbUqvUpgzgYBWJxSHMMwAFcOS1eAarNdEU++4yca7X327vsp3k/BuGui3HvLM9Z1SKmGh6JFbrwpG/XYXgu7us7FAAipNVvbmwe8OT0Qki6wdg94NMiNEETiAq0zAuMi/n0xMWxbBG2bqcBnR9zlzKR0joHOSMvBXTg1wMRE5Hbj62Ceb13J7Ndsk/bEj4YNKipimRuS7EEX1iuWD56mtHgGELs8xWGGNE7fvc9z2uleRyhi8R2GNvlFxCgTgll0z0Je0v8shfQhc1ccpu+9YT0pAGpjlZtwzUDnW1Q+v8Q6U3oewvqC6ofF4hn5dxw0yN1i2xcDkysNA1hsJlQ4uyplga+xs7+2+MC3fNinxZRca+r19e0i/37csVOWIiXt/iYg0PKbS";
-                var result = EncryptVivanto.EncryptRSA(keyEjemplo,
-                "{Json serrializado ejemplo}");
+                string keyEjemplo = "x4zEU9yKqtmdBV1X9zwIW8lwhbjjfpqf9vU/6g7T9nrYxRfVwdJEofzXnRpfCB6s/3InvcMYUOEco91SjjzOVYbDmKn2pvDVnYoXP5h+V/JqHImqWfA+p6ywlvxQxzhiyUvz7ZyLSvvws//Hu8EaaGl6mgJtaQ2B2G4u/+adBdOoHpUNXXhcJvx78js0NMbAnN9sJAC7eXMVCGC1oK/9JsQm3RMNE5OukjAYf7GEdasO7wqAtzMIuPPQSIF1WHxtWaMKpLzbySnHEPPxmKl+oNS/b3EOdGBM5dD/pnAd+G9Uk2aBI4yWG6DdhS6TIXxoxhSgIej17OVf5mVcH/sTTSKybB7UqS2sC4pkMZHMvaecddVEYSW8YwqxkhJc6LRuMNipPrOh0VMWN+RfH/WkL3gK5ylu1OVKAFJcVg4u6P8U0w1Ae/DPFgQW+nlBvzxmk5DQXqyFaBH9lHrzIAQo6gIVTKNyKLyWzK/1gBmNE9yKExQY/AlE4arlS9VmWr8MOiKBAlkHk1n1huBOyzZ0qf/AUxaZjP9zErOFGVf+VkPAZgGakhs9N+AdX1KhuA43utOU2yMLJFstqYl/T+JCJ6MLWZH12UZGKEJ+5jlH1/6iMPyxTEMirrwOHl8DOmrPI2wy53K1a9teREVu0ZFRRjn4dgwLF6yNk7kh4/szLV1lf0f11DO6jSJtdSs+mrr9mBZxLEq8cr1oxb8R8dwlDu2+MC3fNinxZRca+r19e0i/37csVOWIiXt/iYg0PKbS";
+                var test = JsonConvert.SerializeObject(new XIdentify() {rol="GOBERNACIOM",departamento="BOGOTA D.C.",municipio= "BOGOTA D.C." });
+                var result = EncryptVivanto.EncryptRSA(keyEjemplo, test);
             }
                 catch (Exception)
                 {
---
+++ TestDecripts/XIdentify.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestDecripts
+{
+    public class XIdentify
+    {
+        public string rol { get; set; }
+        public string departamento { get; set; }
+        public string municipio { get; set; }
+    }
+}
