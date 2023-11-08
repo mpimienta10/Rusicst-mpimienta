31e3fde5038b600beabc072f7cae53135323b263
Maikol José Martínez Sanjuán <maikol.martinez@softwareone.com>
2023-11-08 16:29:52 -0500

    test repo

--- TestDecripts/Program.cs
+++ TestDecripts/Program.cs
@@ -40,6 +40,7 @@ namespace TestDecripts
             {
                 string keyEjemplo = "x4zEU9yKqtmdBV1X9zwIW8lwhbjjfpqf9vU/6g7T9nrYxRfVwdJEofzXnRpfCB6s/3InvcMYUOEco91SjjzOVYbDmKn2pvDVnYoXP5h+V/JqHImqWfA+p6ywlvxQxzhiyUvz7ZyLSvvws//Hu8EaaGl6mgJtaQ2B2G4u/+adBdOoHpUNXXhcJvx78js0NMbAnN9sJAC7eXMVCGC1oK/9JsQm3RMNE5OukjAYf7GEdasO7wqAtzMIuPPQSIF1WHxtWaMKpLzbySnHEPPxmKl+oNS/b3EOdGBM5dD/pnAd+G9Uk2aBI4yWG6DdhS6TIXxoxhSgIej17OVf5mVcH/sTTSKybB7UqS2sC4pkMZHMvaecddVEYSW8YwqxkhJc6LRuMNipPrOh0VMWN+RfH/WkL3gK5ylu1OVKAFJcVg4u6P8U0w1Ae/DPFgQW+nlBvzxmk5DQXqyFaBH9lHrzIAQo6gIVTKNyKLyWzK/1gBmNE9yKExQY/AlE4arlS9VmWr8MOiKBAlkHk1n1huBOyzZ0qf/AUxaZjP9zErOFGVf+VkPAZgGakhs9N+AdX1KhuA43utOU2yMLJFstqYl/T+JCJ6MLWZH12UZGKEJ+5jlH1/6iMPyxTEMirrwOHl8DOmrPI2wy53K1a9teREVu0ZFRRjn4dgwLF6yNk7kh4/szLV1lf0f11DO6jSJtdSs+mrr9mBZxLEq8cr1oxb8R8dwlDu2+MC3fNinxZRca+r19e0i/37csVOWIiXt/iYg0PKbS";
                 var test = JsonConvert.SerializeObject(new XIdentify() {rol="GOBERNACIOM",departamento="BOGOTA D.C.",municipio= "BOGOTA D.C." });
+
                 var result = EncryptVivanto.EncryptRSA(keyEjemplo, test);
             }
                 catch (Exception)
