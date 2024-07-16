8a2524eda775fbc3803e847419b6c368ed9bd537
mpimienta10 <65187966+mpimienta10@users.noreply.github.com>
2024-07-16 10:44:13 -0500

    Ajuste mensajes de error contraseña

--- Mininterior.RusicstMVC.Presentacion/app/directives/CompararContrasenas.js
+++ Mininterior.RusicstMVC.Presentacion/app/directives/CompararContrasenas.js
@@ -17,7 +17,7 @@ app.directive('validPwd', [function () {
     return {
         require: 'ngModel',
         link: function (scope, elm, attrs, ctrl) {
-            ctrl.$parsers.unshift(function (viewValue) {
+            /*ctrl.$parsers.unshift(function (viewValue) {
                 console.log(viewValue)
                 scope.pwdValidLength = (viewValue && viewValue.length >= 8 ? 'valid' : undefined); // Comprueba la longitud de la cadena.
                 scope.pwdHasLetterMay = (viewValue && /[A-Z]/.test(viewValue)) ? 'valid' : undefined; // Compruebe si la cadena contiene letras mayusculas.
@@ -27,7 +27,15 @@ app.directive('validPwd', [function () {
 
                 if (scope.pwdValidLength && scope.pwdHasLetterMay && scope.pwdHasLetterMin && scope.pwdHasNumber && scope.pwdHasCharacter) {
                     ctrl.$setValidity('pwdValid', true); 
-                    return viewValue; 
+                    return viewValue; */
+            ctrl.$parsers.unshift(function (viewValue) {
+                scope.pwdValidLength = (viewValue && viewValue.length >= 6 ? 'valid' : undefined); // Comprueba la longitud de la cadena.
+                scope.pwdHasLetter = (viewValue && /[A-z]/.test(viewValue)) ? 'valid' : undefined; // Compruebe si la cadena contiene letras.
+                scope.pwdHasNumber = (viewValue && /\d/.test(viewValue)) ? 'valid' : undefined; // Compruebe si la cadena contiene dígitos.
+
+                if (scope.pwdValidLength && scope.pwdHasLetter && scope.pwdHasNumber) { 
+                    ctrl.$setValidity('pwdValid', true); 
+                    return viewValue;
                 } else { 
                     ctrl.$setValidity('pwdValid', false); 
                     return undefined; 
